147365d5baded5635d5bb66ffe64533ba30f3122
Somto M.Ugeh <smugeh@gmail.com>
2018-12-19 08:24:06 +0100

    Added boilerplate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import "./index.css";
+import App from "./App";
+import reducers from "./reducers";
+
+ReactDOM.render(
+	<Provider store={createStore(reducers)}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
