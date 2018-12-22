471ee2aa9e61e528ae0d26f690bd5027b87c48ae
Somto M.Ugeh <smugeh@gmail.com>
2018-12-22 04:40:23 +0100

    Refactored userheader

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,15 +8,15 @@ export class UserHeader extends Component {
 	};
 
 	render() {
-		const user = this.props.users.find(
-			user => user.id === this.props.userId
-		);
+		const { user } = this.props;
 		return user ? <div className="header">{user.name}</div> : null;
 	}
 }
 
-const mapStateToProps = state => {
-	return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+	return {
+		user: state.users.find(user => user.id === ownProps.userId)
+	};
 };
 
 export default connect(
